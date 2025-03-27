2bd9f86c15987797e9354e555a402b615c71204e
Electroded <62696273+Electroded@users.noreply.github.com>
2025-03-27 13:01:49 +0300

    1

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -2,14 +2,12 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public int coinValue = 1;
+    [SerializeField] private int coinValue = 1;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == 7)
         {
-            
-
             Destroy(gameObject);
         }
     }
--- Assets/CoinCollector.cs
+++ Assets/CoinCollector.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CoinCollector : MonoBehaviour
 {
-    private int _coins;
+    private int _currentCoins;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,8 +16,8 @@ public class CoinCollector : MonoBehaviour
 
     private void AddCoin()
     {
-        _coins ++;
+        _currentCoins++;
         
-        print(_coins);
+        print(_currentCoins);
     }
 }
